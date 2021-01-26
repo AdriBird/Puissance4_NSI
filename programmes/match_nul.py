4947db0790396ad77735db3f56ea008ae7765a1c
LGdu92 <leongard2004@gmail.com>
2021-01-26 11:47:30 +0100

    une petite modif

--- programmes/match_nul.py
+++ programmes/match_nul.py
@@ -2,3 +2,5 @@ def match_nul(g):
     for m in range(7):
         if g[0][m]==0:
             return False
+    print("Match nul .Serrez vous la main mais en fait non parce que c'est pas très covid mdr.")
+    return True
