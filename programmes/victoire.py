1208d2eff9701995103b69bc2e6761c8939685c2
LGdu92 <leongard2004@gmail.com>
2021-01-26 11:58:02 +0100

    Update victoire.py

--- programmes/victoire.py
+++ programmes/victoire.py
@@ -1,6 +1,6 @@
 def victoire(g,j):
     if horiz(g,j,l,c)==True or vertic(g,j,l,c)==True or diag(g,j,l,c)==True:
-        print("le joueur ",j," a gagné.On peut tous sauter sur le vainqueur mais en fait non parce que c'est pas très covid mdr.")
+        print("le joueur ",j," a gagné. On peut tous sauter sur le vainqueur mais en fait non parce que c'est pas très covid mdr.")
         return True
     else:
         return False
